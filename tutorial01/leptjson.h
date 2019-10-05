64439c55246b7e618ead16ab0d75ec4f95ec02c2
Quchuanxin <49805484+Quchuanxin@users.noreply.github.com>
2019-10-05 11:14:52 +0800

    Update leptjson.h

--- tutorial01/leptjson.h
+++ tutorial01/leptjson.h
@@ -8,14 +8,16 @@ typedef struct {
 }lept_value;
 
 enum {
-    LEPT_PARSE_OK = 0,
-    LEPT_PARSE_EXPECT_VALUE,
-    LEPT_PARSE_INVALID_VALUE,
-    LEPT_PARSE_ROOT_NOT_SINGULAR
+    LEPT_PARSE_OK = 0,              //无错误
+    LEPT_PARSE_EXPECT_VALUE,        //若json只含有空白
+    LEPT_PARSE_INVALID_VALUE,       //若不是其他三种字面值，返回此错误
+    LEPT_PARSE_ROOT_NOT_SINGULAR    //若一个值之后，在空白之后还有其他字符
 };
 
+/*解析json*/
 int lept_parse(lept_value* v, const char* json);
 
+/*获取返回结果*/
 lept_type lept_get_type(const lept_value* v);
 
 #endif /* LEPTJSON_H__ */
